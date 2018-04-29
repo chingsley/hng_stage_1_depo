58bf31cb051f4332559bb59c658a2d35904120fe
chingsley <chingsleychinonso@gmail.com>
2018-04-29 07:38:25 -0700

    my new commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>my stage1 simpleform project</title>
+    </head>
+    <body>
+        <h1>
+            <?php
+            date_default_timezone_set('UTC');
+            echo date("l");
+            echo "Hello World";
+            ?>
+        </h1>
+           <!-- <iframe style="border: none;" width="1000" height="800" src="https://www.figma.com/embed?embed_host=share&url=https://www.figma.com/file/WK0NUpQiYbvG7ljAGai0DCFi/Untitled" allowfullscreen></iframe>
+           -->
+    </body>
+</html>
